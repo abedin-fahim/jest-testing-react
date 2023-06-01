bfc2091b9f895c62f7e53eb034a32745aa8ab7a4
Joynal Abedin <cfcfahim26@gmail.com>
2023-06-01 23:45:45 +0600

    mock testing added

--- src/components/Async.test.jsx
+++ src/components/Async.test.jsx
@@ -3,6 +3,11 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('Post are AVAILABLE', async () => {
+    // Mocking the fetch method
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
     render(<Async />);
 
     // This has a third arguments, which allows us to set a timeout before running the following line which is 1s by default
